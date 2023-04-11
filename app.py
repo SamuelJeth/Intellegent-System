9f0b79f40afee23433cc926662bee4ccea983771
Samuel Jeth Datiles <114549823+SamuelJeth@users.noreply.github.com>
2023-04-11 21:19:26 +0800

    app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import openai
 import os
 import streamlit as st
 
-"""##### Set up the OpenAI API key"""
-
+# Set up the OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-"""##### # Define a function to generate text using GPT-3"""
-
+# Define a function to generate text using GPT-3
 def generate_text(prompt):
     # Set up the GPT-3 engine and parameters
     engine = "text-davinci-002"
@@ -27,22 +25,14 @@ def generate_text(prompt):
     
     return text
 
-"""###### Define a function to get user input for a destination"""
-
+# Define a function to get user input for a destination
 def get_destination():
     # Prompt the user for a destination
     st.write("Let's plan your next vacation!")
     destination = st.text_input("Where would you like to go?")
     return destination
 
-# Prompt the user for their name
-name = input("What's your name? ")
-
-# Greet the user by name
-print(f"Hi, {name}! I'm here to help you plan your next vacation. Where would you like to go?")
-
-"""###### Define a function to generate suggestions for activities and things to do at the destination"""
-
+# Define a function to generate suggestions for activities and things to do at the destination
 def get_activities(destination):
     # Generate suggestions for activities and things to do at the destination
     prompt = f"Here are some activities and things to do in {destination}:\n"
@@ -54,8 +44,7 @@ def get_activities(destination):
     
     return prompt
 
-"""###### Define a function to generate suggestions for accommodations at the destination"""
-
+# Define a function to generate suggestions for accommodations at the destination
 def get_accommodations(destination):
     # Generate suggestions for accommodations at the destination
     prompt = f"Here are some recommended accommodations in {destination}:\n"
@@ -65,8 +54,7 @@ def get_accommodations(destination):
     
     return prompt
 
-"""###### Define a function to generate a travel itinerary for the destination"""
-
+# Define a function to generate a travel itinerary for the destination
 def get_itinerary(destination):
     # Generate a travel itinerary for the destination
     prompt = f"Here's a sample travel itinerary for your trip to {destination}:\n"
@@ -78,17 +66,19 @@ def get_itinerary(destination):
     
     return prompt
 
+# Create the Streamlit app
+st.title("Plan Your Next Vacation!")
 destination = get_destination()
+if destination:
+    activities_prompt = get_activities(destination)
+    accommodations_prompt = get_accommodations(destination)
+    itinerary_prompt = get_itinerary(destination)
 
-activities_prompt = get_activities(destination)
+    st.subheader("Activities and Things to Do")
+    st.write(activities_prompt)
 
-accommodations_prompt = get_accommodations(destination)
+    st.subheader("Accommodations")
+    st.write(accommodations_prompt)
 
-itinerary_prompt = get_itinerary(destination)
-
-st.write(activities_prompt)
-st.write(accommodations_prompt)
-st.write(itinerary_prompt)
-
-# Say goodbye to the user
-st.write("Thanks for using the vacation planning chatbot. Have a great trip!")
+    st.subheader("Travel Itinerary")
+    st.write(itinerary_prompt)
